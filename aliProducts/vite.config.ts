f613f2eefccecd8b0d2eea5891bb7cd4ecdb8dd0
Charlie  Valderrama <carlosvalderrama@onmail.com>
2024-02-13 19:48:36 -0500

    config: Tailwind configured

--- aliProducts/vite.config.ts
+++ aliProducts/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
   },
 });
